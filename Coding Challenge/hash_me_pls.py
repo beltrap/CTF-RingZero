9498ae4ebe513b00b6925cbc97ab6827011e0bba
Paul Joseph Beltran <pj-beltran@novoelectronique.com>
2022-03-25 10:41:31 -0400

    resolve hash me pls

--- Coding Challenge/hash_me_pls.py
+++ Coding Challenge/hash_me_pls.py
@@ -3,16 +3,15 @@ import urllib.request
 import hashlib
 from bs4 import BeautifulSoup
                                      
+url = 'http://challenges.ringzer0team.com:10013'
 
-
-urlChallenge = 'http://challenges.ringzer0team.com:10013'
-
-soup = BeautifulSoup(urllib.request.urlopen(urlChallenge).read(), features="html.parser")
+soup = BeautifulSoup(urllib.request.urlopen(url).read(), features="html.parser")
 
 message = soup.find("div", {"class": "message"}).contents[2]
+message = message.get_text().replace('\n', '').replace(' ', '')
 
-encode = hashlib.sha512(message.get_text().encode('utf8')).hexdigest()
+encode = hashlib.sha512(message.encode('utf8')).hexdigest()
 
-print(encode)
-
-url = 'http://challenges.ringzer0team.com:10013='
+soup = BeautifulSoup(urllib.request.urlopen(url+'/?r='+encode).read(), features="html.parser")
+flag = soup.find("div", {"class": "alert"}).contents[0]
+print(flag)
